e5609e6dc4bbcf15179efe5a3a1942e0075a36aa
laothes <laothes@users.noreply.github.com>
2025-01-20 18:22:58 +0800

    data augment

--- MoCo/src/data_augment.py
+++ MoCo/src/data_augment.py
@@ -1,6 +1,7 @@
 import torch
 import random
 
+
 def RandomMask(x, ratio):
     # x: [batch,channels,seq_len]
     # randomly mask some data as 0
@@ -19,6 +20,7 @@ def CutOut(x, ratio):
         x[b, :, start:start + cut_length] = 0
     return x
 
+
 def JitterScale(x, jitter_range=0.1, scale_range=(0.5, 2)):
     # x: [batch,channels,seq_len]
     # Add random noise and apply random scaling
@@ -26,11 +28,13 @@ def JitterScale(x, jitter_range=0.1, scale_range=(0.5, 2)):
     scale = torch.rand(x.shape[0], 1, 1) * (scale_range[1] - scale_range[0]) + scale_range[0]
     return (x + jitter) * scale
 
-def PermutationJitter(X, min_len, max_segment):
+
+def PermutationJitter(x, min_len, max_segment, jitter_range=0.1):
     # permutation includes splitting the signal into a random number of segments with a maximum of M and randomly shuffling them
-    batch, channels, seq_len = X.shape
-    X_out = X.clone()
+    batch, channels, seq_len = x.shape
+    x_out = x.clone()
     max_possible_segments = seq_len // min_len
+    jitter = torch.randn_like(x.float()) * jitter_range
 
     for b in range(batch):
         # Calculate number of segments based on min_len
@@ -49,25 +53,27 @@ def PermutationJitter(X, min_len, max_segment):
 
         start = 0
         for end in valid_points:
-            segments.append(X_out[b, :, start:end].clone())
+            segments.append(x_out[b, :, start:end].clone())
             start = end
-        segments.append(X_out[b, :, start:].clone())
+        segments.append(x_out[b, :, start:].clone())
 
         random.shuffle(segments)
 
         current_pos = 0
         for segment in segments:
             segment_length = segment.shape[-1]
-            X_out[b, :, current_pos:current_pos + segment_length] = segment
+            x_out[b, :, current_pos:current_pos + segment_length] = segment
             current_pos += segment_length
 
-    return X_out
+    return x_out + jitter
+
+
 if __name__ == '__main__':
-    a = torch.randint(1, 5, (2, 5, 10), requires_grad=False)
+    a = torch.randint(1, 5, (2, 3, 20), requires_grad=False)
     print(a)
     # mask = RandomMask(a.clone(), 0.3)
     # print(mask)
     # cut_out = CutOut(a.clone(), 0.3)
     # print(cut_out)
     # print(JitterScale(a.clone()))
-    print(PermutationJitter(a.clone(), 3,3))
+    print(PermutationJitter(a.clone(), 3, 5, 0))
